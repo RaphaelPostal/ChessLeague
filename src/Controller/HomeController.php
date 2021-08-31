8e6bc72b67103cd9a8cc16a02b3c37a6c3cafcd8
RaphaelPostal <72035795+RaphaelPostal@users.noreply.github.com>
2021-08-31 17:49:40 +0200

    calculate score

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\GameRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,32 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      * @param GameRepository $gameRepository
+     * @param EntityManagerInterface $entityManager
      * @return Response
      */
-    public function index(GameRepository $gameRepository): Response
+    public function index(GameRepository $gameRepository, EntityManagerInterface $entityManager): Response
     {
         $all_games = $gameRepository->findAll();
+        $pointsRaphael = 0;
+        $pointsConstant = 0;
+
+        foreach ($all_games as $game) {
+            if($game->getWinner() !== null) {
+                if ($game->getWinner()->getName() == 'Raphaël') {
+                    $pointsRaphael ++;
+                } elseif ($game->getWinner()->getName() == 'Constant') {
+                    $pointsConstant ++;
+                }
+            } else {
+                $pointsRaphael += 0.5;
+                $pointsConstant += 0.5;
+            }
+        }
+
         return $this->render('home/index.html.twig', [
             'all_games' => $all_games,
+            'pts_raph' => $pointsRaphael,
+            'pts_cst' => $pointsConstant,
         ]);
     }
 }
